d334e2f8c10a5d5d0be77743266cf85e340bf4f4
Anatoly Scherbakov <altaisoft@gmail.com>
2020-03-02 20:38:23 +0700

    Initial commit

---
+++ platonic_amazon_sqs/queue.py
@@ -0,0 +1,22 @@
+import typing
+from typing_extensions import Deque
+
+
+def some_function(first: int, second: int) -> int:
+    """
+    We use this function as an example for some real logic.
+
+    This is how you can write a doctest:
+
+    .. code:: python
+
+        >>> some_function(2, 3)
+        5
+
+    Enjoy!
+    """
+    return first + second
+
+
+class SQSQueue(typing.Deque):
+    pass
