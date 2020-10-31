77609903dc82fc5e9d2eabf63d165078dec838b0
Anatoly Scherbakov <altaisoft@gmail.com>
2020-10-31 13:55:54 +0700

    #20 Renamed files

--- platonic/sqs/queue/__init__.py
+++ platonic/sqs/queue/__init__.py
@@ -2,7 +2,7 @@ from platonic.sqs.queue.errors import (
     SQSMessageDoesNotExist,
     SQSQueueDoesNotExist,
 )
-from platonic.sqs.queue.input import SQSReceiver
 from platonic.sqs.queue.message import SQSMessage
-from platonic.sqs.queue.output import SQSSender
+from platonic.sqs.queue.receiver import SQSReceiver
+from platonic.sqs.queue.sender import SQSSender
 from platonic.sqs.queue.types import InternalType, ValueType
